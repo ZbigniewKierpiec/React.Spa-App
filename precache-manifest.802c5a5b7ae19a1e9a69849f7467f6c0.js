2395b4afb12d1995baa5132210b4c202d7f9a69b
ZbigniewKierpiec <67008017+ZbigniewKierpiec@users.noreply.github.com>
2020-09-10 10:26:55 +0100

    Updates

---
+++ precache-manifest.802c5a5b7ae19a1e9a69849f7467f6c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9620ab7c0e6a68b0978a9cf8dd482d26",
+    "url": "/React.Spa-App/index.html"
+  },
+  {
+    "revision": "933db5308ec86494fdff",
+    "url": "/React.Spa-App/static/css/main.90a463da.chunk.css"
+  },
+  {
+    "revision": "9302ee01382e8e102761",
+    "url": "/React.Spa-App/static/js/2.83e24dfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React.Spa-App/static/js/2.83e24dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933db5308ec86494fdff",
+    "url": "/React.Spa-App/static/js/main.31e190cb.chunk.js"
+  },
+  {
+    "revision": "7e2da249b69f70379624",
+    "url": "/React.Spa-App/static/js/runtime-main.d776dd20.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/React.Spa-App/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/React.Spa-App/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/React.Spa-App/static/media/header3.d9d36b65.jpg"
+  }
+]);
